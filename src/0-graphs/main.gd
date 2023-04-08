3e66f3c283e843872d5791b993b1588d17d85fb7
Yudi Yamane <yudiyamane@gmail.com>
2023-04-08 16:27:22 -0300

    chore: move arquivos para src/

---
+++ src/0-graphs/main.gd
@@ -0,0 +1,24 @@
+extends SceneTree
+
+func _init() -> void:
+	var g = Graph.new(6)
+	g.add_edge(0, 2)
+	g.add_edge(0, 1)
+	g.add_edge(0, 5)
+	g.add_edge(1, 0)
+	g.add_edge(1, 2)
+	g.add_edge(2, 0)
+	g.add_edge(2, 1)
+	g.add_edge(2, 3)
+	g.add_edge(2, 4)
+	g.add_edge(3, 5)
+	g.add_edge(3, 4)
+	g.add_edge(3, 2)
+	g.add_edge(4, 3)
+	g.add_edge(4, 2)
+	g.add_edge(5, 3)
+	g.add_edge(5, 0)
+	print(g.to_str())
+	g.free()
+	
+	quit()
